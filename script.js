82601d64a762294cf5db9dc120576f62041d8b46
hamzaishfaq7 <hamzaishfaq980@gmail.com>
2021-11-18 21:38:58 -0500

    small cahnges

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const navbarLinks = document.getElementsByClassName('navbarLinks')[0]
 toggle.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+
+
+
+
+
+
+
+const themeSwitch = document.querySelector('input');
+
+themeSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-theme');
+});
